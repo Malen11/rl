247060408cfacae2f4f515b90c91b9eb129686c8
Malen11 <49829166+Malen11@users.noreply.github.com>
2020-06-12 19:35:07 +0700

    Update utils.py

--- agents/rl/utils.py
+++ agents/rl/utils.py
@@ -49,7 +49,7 @@ class ReplayMemory(object):
 
         '''
         #
-        if self.size > self.max_replay_num:
+        if self.size > self.max_replay_num and self.max_replay_num is not -1:
             for key in self.memory.keys():
                 self.memory[key].pop(0)
             self.size -= 1
@@ -74,6 +74,34 @@ class ReplayMemory(object):
             self.memory[key].clear()
         self.size = 0
         
+    def get_samples(self, start = 0, end = -1):
+        '''
+        Получить набор набор n случайных записей из памяти
+
+        Parameters
+        ----------
+        batch_size : int
+            DESCRIPTION.
+
+        Returns
+        -------
+        list
+            DESCRIPTION.
+
+        '''
+        if end is -1:
+            end = self.size
+        #выбираем batch_size записей
+        ids = range(start, end)
+        
+        states = np.asarray([self.memory['state'][i] for i in ids])
+        actions = np.asarray([self.memory['action'][i] for i in ids])
+        rewards = np.asarray([self.memory['reward'][i] for i in ids])
+        states_next = np.asarray([self.memory['next_state'][i] for i in ids])
+        dones = np.asarray([self.memory['done'][i] for i in ids])
+        
+        return {'state': states, 'action':actions, 'reward': rewards, 'next_state': states_next, 'done': dones}
+    
     def get_random_samples(self, batch_size):
         '''
         Получить набор набор n случайных записей из памяти
@@ -99,3 +127,25 @@ class ReplayMemory(object):
         dones = np.asarray([self.memory['done'][i] for i in ids])
         
         return {'state': states, 'action':actions, 'reward': rewards, 'next_state': states_next, 'done': dones}
+    
+class LSTMemory(object):
+    
+    def __init__(self, timesteps, data_shape):
+        self.timesteps = timesteps
+        self.data_shape = data_shape
+        self.lstm_data = []
+        self.reset()
+        
+    def add_data(self, data):
+        assert np.shape(data) == self.data_shape
+        self.lstm_data.pop(0)
+        self.lstm_data.append(data)
+        
+    def size(self):
+        return len(self.lstm_data)
+    
+    def get_data(self):
+        return np.copy(self.lstm_data)
+        
+    def reset(self):
+        self.lstm_data = [np.zeros(self.data_shape) for i in range(self.timesteps)]
